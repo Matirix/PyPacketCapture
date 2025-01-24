6d208188de0f4c8ef8964c65cdcdd3b77863ee79
Matthew <Matirix@live.com>
2025-01-23 23:20:15 -0800

    Finished ipv4, completing tcp

--- packet_parsers.py
+++ packet_parsers.py
@@ -4,7 +4,7 @@ def format_mac(hex_data):
 
 
 def format_hex_to_decimal(hex_data):
-    return '.'.join(map(str, (int(hex_data[i:i+2], 16) for i in range(0, 8, 2))))
+    return '.'.join(str(int(hex_data[i:i+2], 16)) for i in range(0, 8, 2))
 
 
 def parse_ethernet_header(hex_data):
@@ -25,7 +25,7 @@ def parse_ethernet_header(hex_data):
         parse_arp_header(payload)
     elif ether_type == "0800":  # IPv4
         # Then do if statements for ICMP, TCP, UDP headers
-        pass
+        parse_ipv4_header(payload)
     else:
         print(f"  {'Unknown EtherType:':<25} {ether_type:<20} | {int(ether_type, 16)}")
         print("  No parser available for this EtherType.")
@@ -47,4 +47,38 @@ def parse_arp_header(hex_data):
     print(f"  {'Sender IP:':<25} {hex_data[28:36]:<20} | {format_hex_to_decimal(hex_data[28:36])}")
     print(f"  {'Target MAC:':<25} {hex_data[36:48]:<20} | {format_mac(hex_data[36:48])}")
     print(f"  {'Target IP:':<25} {hex_data[48:56]:<20} | {format_hex_to_decimal(hex_data[48:56])}")
+
+
+def parse_ipv4_header(hex_data):
+    print(f"IPv4 Header")
+    print(hex_data)
+    print(f"  {'Version:':<25} {hex_data[:1]:<20} | {int(hex_data[:1],16)}")
+    print(f"  {'Header Length:':<25} {hex_data[1:2]:<20} | {int(hex_data[1:2],16) * 4} bytes")  # A word is 4 bytes.
+    print(f"  {'Total Length:':<25} {hex_data[2:6]:<20} | {int(hex_data[2:6],16)}")
+    print(f"  {'Flags & Frag Offset':<25} {hex_data[6:10]:<20} | {int(hex_data[6:10],16)}")
+    print(f"  {'--Reserved:':<25} {hex_data[10:11]:<20}")
+    print(f"  {'--DF (Do not Fragment):':<25} {hex_data[11:12]:<20} ")
+    print(f"  {'--MF (More Fragments):':<25} {hex_data[12:13]:<20}")
+    print(f"  {'--Fragment Offset:':<25} {hex_data[13:18]:<20} | {int(hex_data[13:18],16)}")
+    print(f"  {'Protocol:':<25} {hex_data[18:20]:<20} | {int(hex_data[18:20],16)}")
+    print(f"  {'Source IP:':<25} {hex_data[20:28]:<20} | {format_hex_to_decimal(hex_data[20:28])}")
+    print(f"  {'Destination IP:':<25} {hex_data[28:36]:<20} | {format_hex_to_decimal(hex_data[28:36])}")
+    parse_tcp_header(hex_data[36:])
+
+
+def parse_tcp_header(hex_data):
+    print(f" TCP Header:")
+    print(hex_data)
+    print(f"  {'Source Port:':<25} {hex_data[:4]:<20} | {int(hex_data[:4],16)}")
+    print(f"  {'Destination Port:':<25} {hex_data[4:8]:<20} | {int(hex_data[4:8],16)}")
+    print(f"  {'Sequence Number:':<25} {hex_data[8:16]:<20} | {int(hex_data[:4],16)}")
+    print(f"  {'Acknowledgement Number:':<25} {hex_data[16:24]:<20} | {int(hex_data[:4],16)}")
+    print(f"  {'Data Offset:':<25} {hex_data[24:25]:<20} | {int(hex_data[:4],16) * 4} bytes")
+    print(f"  {'Reserved':<25} {hex_data[25:28]:<20} | {int(hex_data[:4],16)}")
+    print(f"  {'Flags:':<25} {hex_data[28:39]:<20} | {int(hex_data[:4],16)}")
+    print(f"  {'Window Size:':<25} {hex_data[39:43]:<20} | {int(hex_data[:4],16)}")
+    print(f"  {'Checksum:':<25} {hex_data[43:47]:<20} | {int(hex_data[:4],16)}")
+    print(f"  {'Urgent Pointer:':<25} {hex_data[47:51]:<20} | {int(hex_data[:4],16)}")
+    print(f"  {'Payload (Hex):':<25} {hex_data[51:]:<20}")
+
 
